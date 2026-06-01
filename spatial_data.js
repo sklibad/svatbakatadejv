7749870867f0a5a2885160d23b1d7a98dba186c8
sklibad <74158865+sklibad@users.noreply.github.com>
2026-06-01 19:52:56 +0200

    Add files via upload

---
+++ spatial_data.js
@@ -0,0 +1,9 @@
+var pointLayer = {
+    "type": "FeatureCollection",
+    "name": "svatba",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": 1, "typ": "svatba", "url": "https://www.hoffmanuv-dvur.cz/" }, "geometry": { "type": "Point", "coordinates": [ 14.581995693708250, 50.14177532912956 ] } },
+    ]
+    }
+    ;    
